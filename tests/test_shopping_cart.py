64c9feffbbe1ca5a5ef8dee358c8f3902f302779
Valerij-Marusyak <mars2vm@gmail.com>
2022-11-07 23:29:14 +0200

    everything is ok again

--- tests/test_shopping_cart.py
+++ tests/test_shopping_cart.py
@@ -1,7 +1,4 @@
 import unittest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
 from page_object.shopping_cart_page import ShoppingCart
 from page_object.product_page import ProductPage
 from webdriver_factory import WebDriverFactory
